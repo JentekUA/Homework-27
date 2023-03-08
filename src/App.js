f0b5db907f4c3e602d7cbf38b62350539b1d841c
oleksandr aleksashyn <oleksandr.aleksashyn@delasport.com>
2023-03-08 09:13:33 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './components/button'
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button text="test"/>
       </header>
     </div>
   );
